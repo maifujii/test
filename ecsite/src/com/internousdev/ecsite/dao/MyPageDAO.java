7d27bff90d376d28c3311502cfb27d1c4d8c76d7
maifujii <b1100136@gl.aiu.ac.jp>
2018-09-07 21:04:56 +0900

    2018/9/7

--- ecsite/src/com/internousdev/ecsite/dao/MyPageDAO.java
+++ ecsite/src/com/internousdev/ecsite/dao/MyPageDAO.java
@@ -17,19 +17,7 @@ public class MyPageDAO {
 		(String item_transaction_id, String user_master_id) throws SQLException{
 		ArrayList<MyPageDTO> myPageDTO = new ArrayList<MyPageDTO>();
 		String sql=
-			"SELECT"
-				+ "ubit.id, iit.item_name, ubit.total_price, ubit.total_count,"
-				+ "ubit.pay, ubit.insert_date"
-			+ "FROM"
-				+ "user_buy_item_transaction ubit"
-			+ "LEFT JOIN"
-				+ "item_info_transaction iit"
-			+ "ON"
-				+ "ubit.item_transaction_id=iit.id"
-			+ "WHERE"
-				+ "ubit.item_transaction_id=? AND ubit.user_master_id=?"
-			+ "ORDER BY"
-				+ "insert_date DESC";
+			"SELECT ubit.id, iit.item_name, ubit.total_price, ubit.total_count, ubit.pay, ubit.insert_date FROM user_buy_item_transaction ubit LEFT JOIN item_info_transaction iit ON ubit.item_transaction_id=iit.id WHERE ubit.item_transaction_id=? AND ubit.user_master_id=? ORDER BY insert_date DESC";
 		try{
 			PreparedStatement preparedStatement = connection.prepareStatement(sql);
 			preparedStatement.setString(1,item_transaction_id);
@@ -58,10 +46,7 @@ public class MyPageDAO {
 	public int buyItemHistoryDelete
 	(String item_transaction_id, String user_master_id) throws SQLException{
 		String sql=
-				"DELETE FROM"
-				+ "user_buy_item_transaction"
-			+ "WHERE"
-				+ "item_transaction_id =? AND user_master_id=?";
+				"DELETE FROM user_buy_item_transaction WHERE item_transaction_id =? AND user_master_id=?";
 		PreparedStatement preparedStatement;
 		int result=0;
 		try{
